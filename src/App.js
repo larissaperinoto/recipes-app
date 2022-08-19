52acbf62e8574ac892f5b2cbe3ad160f00b5b9c7
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-08-19 18:21:52 -0300

    Rotas definidas e adicionadas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      {/* <Route exact path="/" component={} /> */}
+      {/* <Route exact path="/foods" component={} /> */}
+      {/* <Route exact path="/drinks" component={} /> */}
+      {/* <Route exact path="/foods/{id-da-receita}" component={} /> */}
+      {/* <Route exact path="/drinks/{id-da-receita}" component={} /> */}
+      {/* <Route exact path="/foods/{id-da-receita}/in-progress" component={} /> */}
+      {/* <Route exact path="/drinks/{id-da-receita}/in-progress" component={} /> */}
+      {/* <Route exact path="/profile" component={} /> */}
+      {/* <Route exact path="/done-recipe" component={} /> */}
+      {/* <Route exact path="/favorite-recipes" component={} /> */}
+    </Switch>
   );
 }
 
