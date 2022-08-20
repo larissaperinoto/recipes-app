af38fb0d532e4e08ede6326bc401bf7b9af0d01c
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-08-20 14:23:30 -0300

    Requisito 4 realizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,8 +4,8 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { withRouter, renderWithRouter, renderWithContext } from '../helpers/renderWithRouterContext';
 import Footer from '../components/Footer';
-import drinkIcon from '../images/drinkIcon.svg';
-import mealIcon from '../images/mealIcon.svg';
+// import drinkIcon from '../images/drinkIcon.svg';
+// import mealIcon from '../images/mealIcon.svg';
 
 describe('Componente Footer', () => {
     test('Deve renderizar  a página Drinks', () => {
@@ -34,13 +34,31 @@ describe('Componente Footer', () => {
 
         userEvent.click(screen.getByTestId('drinks-bottom-btn'));
         const path = history.location.pathname;
-        expect(path).toBe('/drinks')
         userEvent.click(screen.getByTestId('food-bottom-btn'));
-        expect(withRouter(<Footer />, '/foods')).toBe('/foods');
+        expect(path).toBe('/drinks');
+    
+    });
 
-        expect(screen.getByRole('src', drinkIcon)).toBeInTheDocument();
-
+    test('Deve renderizar os data-testids do Login', () => {
+        renderWithRouter(<App />);
+        expect(screen.getByTestId('email-input')).toBeInTheDocument();
+        expect(screen.getByTestId('password-input')).toBeInTheDocument();
+        expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
         
+    });
+
+    test('Deve verificar se o botão está habilidado quando o campo email e senha estão corretos', () => {
+        renderWithRouter(<App />);
+        const emailInput = screen.getByTestId('email-input');
+        const senhaInput = screen.getByTestId('password-input');
+        const loginSubmitBtn = screen.getByTestId('login-submit-btn');
+
+        expect(loginSubmitBtn.disabled).toBe(true);
+    
+        userEvent.type(emailInput, 'email@mail.com');
+        userEvent.type(senhaInput, '1234567');
+
+        expect(loginSubmitBtn.disabled).toBe(false);
         
     });
 });
