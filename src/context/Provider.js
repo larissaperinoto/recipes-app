4a4404e6a758768f49d6cea12769eb198af9c2e2
Larissa Perinoto <perinotolarissa@gmail.com>
2022-08-23 16:35:48 -0300

    [Grupo 21] - Requisito 24.

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,9 +5,19 @@ import Context from './Context';
 function Provider({ children }) {
   const [userEmail, setUserEmail] = useState({ user: { email: '', isValid: false } });
   const [userSenha, setUserSenha] = useState({ senha: { isValid: false } });
+  const [recipeType, setRecipeType] = useState('foods');
+
+  const value = {
+    userEmail,
+    setUserEmail,
+    userSenha,
+    setUserSenha,
+    recipeType,
+    setRecipeType,
+  };
 
   return (
-    <Context.Provider value={ { userEmail, setUserEmail, userSenha, setUserSenha } }>
+    <Context.Provider value={ value }>
       {children}
     </Context.Provider>
   );
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import Context from '../context/Context';
+import {
+  requestMealWithId,
+  requestDrinkWithId } from '../services/requestMealsAndDrinksAPI';
 
-function RecipeDetails() {
+function RecipeDetails({ history }) {
+  const [recipeDetails, setRecipeDetails] = useState();
+  const { recipeType } = useContext(Context);
+  console.log(recipeDetails);
+
+  const { location: { pathname } } = history;
+  const id = pathname.split('/')[2];
+  console.log(id);
+
+  useEffect(() => {
+    const requestData = async () => {
+      if (recipeType === 'foods') {
+        const data = await requestMealWithId(id);
+        setRecipeDetails(data);
+      }
+      const data = await requestDrinkWithId(id);
+      setRecipeDetails(data);
+    };
+    requestData();
+  }, []);
+
   return (
     <h1>Hello World</h1>
   );
 }
+
+RecipeDetails.propTypes = {
+  history: PropTypes.node,
+}.isRequired;
 
 export default RecipeDetails;
