2f4e9a9d29bc250a502da0621ecae6ee670d03c9
Harumi Maeda <harumiumaeda@gmail.com>
2022-08-26 17:38:56 -0300

    Corrige importação dos arquivos do Header

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
+// Headers
+export { default as Header } from './headerComponents/Header';
+export { default as HeaderSearchIcon } from './headerComponents/HeaderSearchIcon';
+export { default as SearchBar } from './headerComponents/SearchBar';
 export { default as DrinkList } from './DrinkList';
 export { default as Footer } from './Footer';
-// export { default as Header } from './Header';
-// export { default as HeaderSearchIcon } from './HeaderSearchIcon';
 export { default as MealList } from './MealList';
-// export { default as SearchBar } from './SearchBar';
 export { default as Slider } from './Slider';
 export { default as FavoriteButton } from './FavoriteButton';
 export { default as StartRecipeButton } from './StartRecipeButton';
