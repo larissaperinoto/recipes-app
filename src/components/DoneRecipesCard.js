0228025d386579095f0abde59045aa7b29bb23a5
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-01 10:38:18 -0300

    [Grupo 21] - Requisitos 49 e 56.

--- src/components/DoneRecipesCard.js
+++ src/components/DoneRecipesCard.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FavoriteAndShareButtons from './FavoriteAndShareButtons';
+import './styles/image.css';
 
 export default function DoneRecipesCard({ doneRecipes }) {
-  // const history = useHistory();
-
-  /* const handleClick = (id, type) => {
-    history.push(`/${type}s/${id}`);
-  }; */
-
   return (
     doneRecipes && doneRecipes.map((recipe, index) => (
       <div id="card" key={ index }>
-        <img
-          src={ recipe.image }
-          data-testid={ `${index}-horizontal-image` }
-          alt={ recipe.name }
-          // onClick={ () => handleClick(recipe.id, recipe.type) }
-        />
+
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <img
+            src={ recipe.image }
+            data-testid={ `${index}-horizontal-image` }
+            alt={ recipe.name }
+            className="img"
+          />
+        </Link>
+
         <h4
           data-testid={ `${index}-horizontal-top-text` }
         >
           { recipe.type === 'food'
             ? `${recipe.nationality} - ${recipe.category}` : recipe.alcoholicOrNot }
         </h4>
-        <h3 data-testid={ `${index}-horizontal-name` }>
-          { recipe.name }
-        </h3>
+
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <h3 data-testid={ `${index}-horizontal-name` }>
+            { recipe.name }
+          </h3>
+        </Link>
+
         <p data-testid={ `${index}-horizontal-done-date` }>
           { recipe.doneDate }
         </p>
+
         <FavoriteAndShareButtons
           type={ recipe.type }
           id={ recipe.id }
@@ -36,6 +41,7 @@ export default function DoneRecipesCard({ doneRecipes }) {
           testIdFavorite={ `${index}-horizontal-favorite-btn` }
           replace="done-recipes"
         />
+
         { recipe.tags.length > 0 && recipe.tags.map((item, i) => (
           <p
             data-testid={ `${index}-${item}-horizontal-tag` }
@@ -44,6 +50,7 @@ export default function DoneRecipesCard({ doneRecipes }) {
             { item }
           </p>
         ))}
+
       </div>
     ))
   );
--- src/components/FavoritesRecipesCard.js
+++ src/components/FavoritesRecipesCard.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import clipboardCopy from 'clipboard-copy';
+
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+import './styles/image.css';
 
 export default function FavoriteRecipesCard({ favoriteRecipes }) {
   const [copy, setCopy] = useState(false);
-
-  // const history = useHistory();
 
   function copyLink(id, type) {
     clipboardCopy(window.location.href.replace('/favorite-recipes', `/${type}s/${id}`));
@@ -17,20 +17,29 @@ export default function FavoriteRecipesCard({ favoriteRecipes }) {
   return (
     favoriteRecipes && favoriteRecipes.map((recipe, index) => (
       <div id="card" key={ index }>
-        <img
-          src={ recipe.image }
-          data-testid={ `${index}-horizontal-image` }
-          alt={ recipe.name }
-        />
+
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <img
+            src={ recipe.image }
+            data-testid={ `${index}-horizontal-image` }
+            alt={ recipe.name }
+            className="img"
+          />
+        </Link>
+
         <h4
           data-testid={ `${index}-horizontal-top-text` }
         >
           { recipe.type === 'food'
             ? `${recipe.nationality} - ${recipe.category}` : recipe.alcoholicOrNot }
         </h4>
-        <h3 data-testid={ `${index}-horizontal-name` }>
-          { recipe.name }
-        </h3>
+
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <h3 data-testid={ `${index}-horizontal-name` }>
+            { recipe.name }
+          </h3>
+        </Link>
+
         <button
           type="button"
           onClick={ () => copyLink(recipe.id, recipe.type) }
@@ -41,7 +50,9 @@ export default function FavoriteRecipesCard({ favoriteRecipes }) {
             alt="shareIcon"
           />
         </button>
+
         { copy && <p>Link copied!</p> }
+
         <button
           type="button"
         >
@@ -51,6 +62,7 @@ export default function FavoriteRecipesCard({ favoriteRecipes }) {
             alt="shareIcon"
           />
         </button>
+
       </div>
     ))
   );
