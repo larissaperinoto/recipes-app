f0b28f82b48d71c318a9fb12b93176f210a55ff7
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-22 21:26:02 -0300

    style: insere estilo na página de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Button, Container, Stack, Typography } from '@mui/material';
 
 import { Footer, Header } from '../components/index';
 
 export default function Profile() {
   const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
 
   const handleLogoutButton = () => {
     localStorage.clear();
@@ -14,22 +16,34 @@ export default function Profile() {
   return (
     <>
       <Header title="Profile" />
-      <h1>{ JSON.parse(localStorage.getItem('user')).email }</h1>
-
-      <a href="/done-recipes">
-        <button type="button">Done Recipes</button>
-      </a>
-
-      <a href="/favorite-recipes">
-        <button type="button">Favorite Recipes</button>
-      </a>
-
-      <button
-        type="button"
-        onClick={ handleLogoutButton }
-      >
-        Logout
-      </button>
+      <Container>
+        <Stack alignItems="center">
+          <Typography variant="h6" sx={ { m: 2 } }>
+            { email }
+          </Typography>
+          <Button
+            type="button"
+            color="secondary"
+            onClick={ () => history.push('/done-recipes') }
+          >
+            Done Recipes
+          </Button>
+          <Button
+            type="button"
+            color="secondary"
+            onClick={ () => history.push('/favorite-recipes') }
+          >
+            Favorite Recipes
+          </Button>
+          <Button
+            type="button"
+            color="secondary"
+            onClick={ () => handleLogoutButton() }
+          >
+            Logout
+          </Button>
+        </Stack>
+      </Container>
       <Footer />
     </>
   );
